3161f92d6eb46bfaeb195cd59174d4b52fb26ab5
Lee Shinhee <shlee2227@gmail.com>
2024-06-04 22:47:16 +0900

    [EDIT] Edit keras server return data format

--- python_server_keras.py
+++ python_server_keras.py
@@ -1,3 +1,4 @@
+from cv2 import threshold
 from flask import Flask, request, jsonify
 import requests
 from io import BytesIO
@@ -7,43 +8,58 @@ import tensorflow as tf
 app = Flask(__name__)
 
 model = tf.keras.applications.resnet50.ResNet50(weights='imagenet')
+treshold = 0.01
 
 def preprocess_image(image):
-    image = image.resize((256, 256))
-    image = tf.keras.preprocessing.image.img_to_array(image)
-    image = tf.keras.applications.resnet50.preprocess_input(image)
-    image = tf.expand_dims(image, axis=0)
-    return image
+  image = image.resize((224, 224))
+  image = tf.keras.preprocessing.image.img_to_array(image)
+  image = tf.keras.applications.resnet50.preprocess_input(image)
+  image = tf.expand_dims(image, axis=0)
+  return image
 
-def decode_predictions(predictions):
-    return tf.keras.applications.resnet50.decode_predictions(predictions, top=1)[0]
 
+def filtered_predictions(predictions):
+  # prediction decoding, reformating, filtering
+  decoded_predictions = tf.keras.applications.resnet50.decode_predictions(predictions, top=10)[0]
+  formated_predictions = [{"label": label, "score": float(score)} for _, label, score in decoded_predictions]
+  filtered_predictions = [pred for pred in formated_predictions if pred["score"] >= treshold]
+
+  # result 기본 snippet
+  result = {"classification_number" : len(filtered_predictions), # 섞인 종 갯수
+            "Information" : "양육정보 추가예정"} # 양육정보 
+  
+  # result에 rkr prediction 아이템 추가 
+  for idx, prediction_item in enumerate(filtered_predictions):
+    label = prediction_item["label"]
+    score = prediction_item["score"]
+    result[f"classification_{idx+1:02}"] = [{"name" : label, "value" : score}] # 종 이름, 비율
+  return result    
+  
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        data = request.json
-        image_url = data.get('image_url')
+  try:
+    data = request.json
+    image_url = data.get('image_url')
 
-        if not image_url:
-            return jsonify({"error": "Image URL is required"}), 400
+    if not image_url:
+      return jsonify({"error": "Image URL is required"}), 400
 
-        response = requests.get(image_url)
-        if response.status_code != 200:
-            return jsonify({"error": "Failed to download image"}), 400
+    response = requests.get(image_url)
+    if response.status_code != 200:
+      return jsonify({"error": "Failed to download image"}), 400
 
-        image = Image.open(BytesIO(response.content))
-        if image.format not in ['JPEG', 'PNG']:
-            return jsonify({"error": "Unsupported image format"}), 400
+    image = Image.open(BytesIO(response.content))
+    if image.format not in ['JPEG', 'PNG']:
+      return jsonify({"error": "Unsupported image format"}), 400
+    
+    processed_image = preprocess_image(image)
+    predictions = model.predict(processed_image)
+    results = filtered_predictions(predictions)
 
-        processed_image = preprocess_image(image)
-        predictions = model.predict(processed_image)
-        decoded_predictions = decode_predictions(predictions)
+    return jsonify(results)
 
-        results = [{"label": label, "score": float(score)} for _, label, score in decoded_predictions]
-        return jsonify({"predictions": results})
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+  except Exception as e:
+    return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5500)
+  app.run(host='0.0.0.0', port=5500, debug=True)
--- python_server_robo_api.py
+++ python_server_robo_api.py
@@ -2,53 +2,46 @@ from flask import Flask, request, jsonify
 import requests
 from io import BytesIO
 from PIL import Image
-import tensorflow as tf
-import dotenv
-import os
-from IPython import display
 from roboflow import Roboflow
 from PIL import Image
+import dotenv
+import os
 
 dotenv.load_dotenv()
 API_KEY = os.getenv("ROBOFLOW_REF_MODEL_API")
 
+app = Flask(__name__)
 
 rf = Roboflow(API_KEY)
 project = rf.workspace().project("dog_breed_simple")
 model = project.version(1).model
 
-app = Flask(__name__)
-
-
 def preprocess_image(image):
-    image = image.resize((256, 256))
-    temp_image_path = "/tmp/temp_image.jpg"
-    image.save(temp_image_path)
-    return temp_image_path
-
-# def decode_predictions(predictions):
-#     return tf.keras.applications.resnet50.decode_predictions(predictions, top=1)[0]pip 
+  image = image.resize((256, 256))
+  temp_image_path = "/tmp/temp_image.jpg"
+  image.save(temp_image_path)
+  return temp_image_path
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        data = request.json
-        image_url = data.get('image_url')
+  try:
+    data = request.json
+    image_url = data.get('image_url')
 
-        if not image_url:
-            return jsonify({"error": "Image URL is required"}), 400
+    if not image_url:
+      return jsonify({"error": "Image URL is required"}), 400
 
-        response = requests.get(image_url)
-        if response.status_code != 200:
-            return jsonify({"error": "Failed to download image"}), 400
-        # print(response.json)
-        image = Image.open(BytesIO(response.content))
-        temp_image_path = preprocess_image(image)
-        predictions = model.predict(temp_image_path)
-        return predictions.json()
+    response = requests.get(image_url)
+    if response.status_code != 200:
+      return jsonify({"error": "Failed to download image"}), 400
+    # print(response.json)
+    image = Image.open(BytesIO(response.content))
+    temp_image_path = preprocess_image(image)
+    predictions = model.predict(temp_image_path)
+    return predictions.json()
 
-    except Exception as e:
-        return jsonify({"error": e}), 500
+  except Exception as e:
+    return jsonify({"error": e}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5500)
+  app.run(host='0.0.0.0', port=5500)
